1f72f121fdcb11e5af7b2a5e7f788ae08dfdf740
EC2 Default User <maharo_keina@icloud.com>
2021-08-30 06:21:02 +0000

    できたとこまで

--- app.rb
+++ app.rb
@@ -8,3 +8,52 @@ before do
     Count.create(number: 0)
   end
 end
+
+
+get '/' do
+  @number = Count.first.number
+  erb :index
+end
+
+
+
+post '/plus' do
+ count = Count.first
+count.number =count.number + 1
+count.save
+redirect'/'
+end
+
+post '/minus' do
+  count =Count.first
+  count.number =count.number - 1
+  count.save
+  redirect'/'
+end
+
+post '/clear' do
+  count=Count.first
+  count.number= count.number - count.number
+  count.save
+  redirect'/'
+end
+
+post '/kakeru' do
+  count=Count.first
+  count.number=count.number + count.number
+  count.save
+  redirect'/'
+end
+
+post '/waru' do
+  count=Count.first
+  count.number=count.number - count.number/2
+  count.save
+  redirect'/'
+end
+
+
+get '//plus' do
+  @number = Count.first.number
+  erb :index
+end
